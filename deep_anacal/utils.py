03f185436dc54e20ab45577e94d425f8fb1636d5
Andy Park <chanhyup@andrew.cmu.edu>
2025-01-01 22:50:47 -0500

    Update utils.py

--- deep_anacal/utils.py
+++ deep_anacal/utils.py
@@ -43,13 +43,30 @@ def setup_custom_logger(verbose=False):
     return logger
 
 
-def get_e_w(*, acal_res, wname, wgname, ename, egname):
-    shear_res = dict()
-    for i, sign in enumerate(["plus", "minus"]):
-        shear_res[sign] = dict()
-        for quant in [wname, wgname, ename, egname]:
-            shear_res[sign][quant] = acal_res[i][quant]
-    return shear_res
+def get_e_w(*, acat, component=1, force_detection=False):
+    ename = f"fpfs_e{component}"
+    wname = "fpfs_w"
+    if isinstance(acat, list):
+        return [
+            (cat[ename], cat[wname] if not force_detection else 1.0) for cat in acat
+        ]
+    elif isinstance(acat, np.ndarray):
+        return acat[ename], acat[wname] if not force_detection else 1.0
+    else:
+        raise ValueError("acal_res in 'get_e_w' must be of type 'list' or 'np.ndarray'")
+
+
+# TODO - Add selection weights
+def get_R(*, acat, component=1, force_detection=False):
+    egname = f"fpfs_de{component}_dg{component}"
+    wgname = f"fpfs_dw_dg{component}"
+    e_w = get_e_w(acat=acat, component=component, force_detection=force_detection)
+    if isinstance(e_w, list):
+        return [cat[wgname] * e + cat[egname] * w for cat, (e, w) in zip(acat, e_w)]
+    else:
+        e, w = e_w
+        R = acat[wgname] * e + acat[egname] * w
+        return R
 
 
 # TODO - Compute bias from multiple realizations
@@ -60,36 +77,31 @@ def compute_m_and_c(
     true_shear=0.02,
     force_detection=False,
 ):
-    wname = "fpfs_w"
-    wgname = f"fpfs_dw_dg{component}"
-    ename = f"fpfs_e{component}"
-    egname = f"fpfs_de{component}_dg{component}"
-    shear_res = get_e_w(
-        acal_res=acal_res, wname=wname, wgname=wgname, ename=ename, egname=egname
+    if isinstance(acal_res, dict):
+        cat1 = acal_res["deep"]
+        cat2 = acal_res["wide"]
+    elif isinstance(acal_res, list):
+        cat1 = acal_res
+        cat2 = acal_res
+    else:
+        raise ValueError("acal_res must be of type 'dict' or 'list'")
+
+    (e_plus, w_plus), (e_minus, w_minus) = get_e_w(
+        acat=cat1, component=component, force_detection=force_detection
     )
-    if force_detection:
-        for sign in ["plus", "minus"]:
-            shear_res[sign][wname] = 1.0
-            shear_res[sign][wgname] = 0.0
+    (R_plus, R_minus) = get_R(
+        acat=cat2, component=component, force_detection=force_detection
+    )
 
-    def _get_stuff(shear_res, sign, key):
-        return shear_res[sign][key]
-
-    w_plus = _get_stuff(shear_res, "plus", wname)
-    e_plus = _get_stuff(shear_res, "plus", ename)
-    wg_plus = _get_stuff(shear_res, "plus", wgname)
-    eg_plus = _get_stuff(shear_res, "plus", egname)
-    R_plus = wg_plus * e_plus + w_plus * eg_plus
-
-    w_minus = _get_stuff(shear_res, "minus", wname)
-    e_minus = _get_stuff(shear_res, "minus", ename)
-    wg_minus = _get_stuff(shear_res, "minus", wgname)
-    eg_minus = _get_stuff(shear_res, "minus", egname)
-    R_minus = wg_minus * e_minus + w_minus * eg_minus
+    if e_plus.sum() < e_minus.sum():
+        e_plus, e_minus = e_minus, e_plus
+        w_plus, w_minus = w_minus, w_plus
+        R_plus, R_minus = R_minus, R_plus
 
     num1 = np.sum(w_plus * e_plus) - np.sum(w_minus * e_minus)
     num2 = np.sum(w_plus * e_plus) + np.sum(w_minus * e_minus)
     denom = np.sum(R_plus) + np.sum(R_minus)
     mbias = num1 / denom / true_shear - 1
     cbias = num2 / denom
+
     return mbias, cbias
