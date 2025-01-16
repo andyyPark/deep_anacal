e41785e31e600a4c5af4bd630ef6b98bc110c820
Andy Park <chanhyup@andrew.cmu.edu>
2025-01-16 17:41:07 -0500

    Move fpfs measurement to deep_anacal.py

--- deep_anacal/deep_anacal.py
+++ deep_anacal/deep_anacal.py
@@ -9,6 +9,33 @@ def get_max_smooth_scale(*, fwhm_w, fwhm_d, fpfs_config):
         fpfs_smooth_scale = fac * fpfs_config
     return max(fwhm_w, fwhm_d, fpfs_smooth_scale) / fac
 
+def measure_eR(*, gal_array, psf_array, noise_array, noise_variance, fpfs_config, scale, detection, 
+               component=1, force_detection=True):
+    acal_res = anacal.fpfs.process_image(
+        fpfs_config=fpfs_config,
+        pixel_scale=scale,
+        mag_zero=30.0,
+        gal_array=gal_array,
+        psf_array=psf_array,
+        noise_array=noise_array,
+        noise_variance=noise_variance,
+        detection=detection,
+    )
+    ename = f"fpfs_e{component}"
+    egname = f"fpfs_de{component}_dg{component}"
+    wname = "fpfs_w"
+    wgname = f"fpfs_dw_dg{component}"
+    if force_detection:
+        return {
+            "e": acal_res[ename],
+            "R": acal_res[egname],
+        }
+    else:
+        return {
+            "e": acal_res[wname] * acal_res[ename],
+            "R": acal_res[ename] * acal_res[wgname] + acal_res[wname] * acal_res[egname]
+        }
+
 def match_noise(*, deep_noise_fac, noise_std_wide, noise_array_wide, do_rotate=False):
     if deep_noise_fac > 1.0:
         raise ValueError(
